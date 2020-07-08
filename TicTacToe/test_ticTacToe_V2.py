d5d51c06acb9ca548c9ac0f9960551f54cf05b47
Ketan Patil <ketan55patil@gmail.com>
2020-07-08 15:26:59 -0400

    Refactored tests for v2

--- TicTacToe/test_ticTacToe_V2.py
+++ TicTacToe/test_ticTacToe_V2.py
@@ -1,50 +1,29 @@
 from .ticTacToe_V2 import PlayTicTacToe
-from ddt import ddt, data
+from ddt import ddt, data, unpack
 import unittest
 
 
 @ddt
 class TestTicTacToeV2(unittest.TestCase):
 
-    def play_game(self, my_input):
+    @data([['a'], ['invalid']],
+          [[1, 'a'], [True, 'invalid']],
+          [['!', 'a'], ['invalid', 'invalid']],
+          [[-1], ['invalid']],
+          [[1, 1], [True, 'overwrite']],
+          [[1, 4, 2, 5, 3], [True, True, True, True, 'win']],
+          [[4, 1, 5, 2, 6], [True, True, True, True, 'win']],
+          [[7, 4, 8, 5, 9], [True, True, True, True, 'win']],
+          [[1, 1, 4, 2, 5, 3], [True, 'overwrite', True, True, True, 'win']],
+          [[1, 2, 4, 5, 7], [True, True, True, True, 'win']],
+          [[2, 1, 5, 4, 8], [True, True, True, True, 'win']],
+          [[3, 2, 6, 5, 9], [True, True, True, True, 'win']],
+          [[1, 2, 5, 6, 9], [True, True, True, True, 'win']],
+          [[7, 8, 5, 6, 3], [True, True, True, True, 'win']],
+          [[1, 5, 9, 2, 8, 7, 3, 6, 4], [True, True, True, True, True, True, True, True, 'draw']],
+          [[0], [False]])
+    @unpack
+    def test_overwrite_value(self, user_input, output):
         game = PlayTicTacToe()
-        input_sequence = my_input
-        for item in input_sequence:
-            play_status = game.continue_playing(item)
-        return play_status
-
-    @data(['a'], [1, 'a'], ['!', 'a'], [-1])
-    def test_invalid_input(self, value):
-        assert self.play_game(value) == 'invalid'
-
-    @data([1, 1])
-    def test_overwrite_value(self, value):
-        assert self.play_game(value) == 'overwrite'
-
-    # def test_won_row(self, player, row):
-    @data([1, 4, 2, 5, 3], [4, 1, 5, 2, 6, 3], [7, 4, 8, 5, 9], [1, 1, 4, 2, 5, 3])
-    def test_won_row(self, value):
-        assert self.play_game(value) == 'win'
-
-    # def test_won_col(self, player, col):
-    @data([1, 2, 4, 5, 7], [2, 1, 5, 4, 8], [3, 2, 6, 5, 9])
-    def test_won_col(self, value):
-        assert self.play_game(value) == 'win'
-
-    # def test_won_diag(self, player, diag):
-    @data([1, 2, 5, 6, 9], [7, 8, 5, 6, 3])
-    def test_won_diag(self, value):
-        """ diag could be lr or rl """
-        assert self.play_game(value) == 'win'
-
-    @data([1, 5, 9, 2, 8, 7, 3, 6, 4])
-    def test_draw(self, value):
-        assert self.play_game(value) == 'draw'
-
-    @data([0])
-    def test_quit_game(self, value):
-        assert self.play_game(value) == False
-
-
-
-
+        for index, value in enumerate(user_input):
+            assert game.continue_playing(value) == output[index]
